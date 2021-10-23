64eb8de462cb7394b4b7adcea4097ebb157cdb21
h0ss3in88 <mohammad.hussein.taherian@gmail.com>
2021-10-23 15:45:16 +0330

    application initialized successfully

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express,{Express,Request,Response,NextFunction} from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT: number= parseInt(process.env.PORT!,10) || 3600;
+let app: Express = express();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("PORT",PORT);
+app.get('/',(req:Request, res:Response, next:NextFunction) => {
+    return res.status(200).json("{'hello': 'world' }");
+});
+app.listen(app.get("PORT"), () => {
+    console.log(`Running on ${PORT} ⚡`);
+});
